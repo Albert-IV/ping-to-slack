df30ab3b209be36ff3095b299a6996a3ee766b30
Albert Engelbrecht <dropped.on.japan@gmail.com>
2016-03-19 16:35:59 -0500

    error catching

--- app.js
+++ app.js
@@ -12,3 +12,8 @@ client.on('online', function() {
 client.on('stanza', function(stan) {
 	console.dir(stan);
 });
+
+client.on('error', function(e) {
+	console.error(e.stack);
+	process.exit(0);
+})
