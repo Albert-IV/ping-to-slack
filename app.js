83ed996808410b6216bcc889d60b7530a83fcb1f
Albert Engelbrecht <dropped.on.japan@gmail.com>
2016-03-20 14:42:09 -0500

    Fix Names
    This would be much easier if i had a locally working copy.  Or knew VIM
    really well.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var auth = require('./auth.json');
 
-var pingClient = require('./ping')(auth.ping);
-var slackClient = require('./slack')(auth.slack);
+var pingClient = require('./pingClient')(auth.ping);
+var slackClient = require('./slackClient')(auth.slack);
 
 pingClient.on('stanza', function(stan) {
 	if( stan.attrs.type == "get" && stan.children[0].name == "ping" ) {
